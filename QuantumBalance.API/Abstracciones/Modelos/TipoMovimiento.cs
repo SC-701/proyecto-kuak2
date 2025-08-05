05293d92ef5e2248ce50e6da4232e46b9fbfd9f8
Sergio Mauricio Montenegro Pérez <156485337+sergioomp@users.noreply.github.com>
2025-08-04 23:11:29 -0600

    Nuevos modelos

---
+++ QuantumBalance.API/Abstracciones/Modelos/TipoMovimiento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Modelos
+{
+    public class TipoMovimientoBase
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(255)]
+        public string Nombre { get; set; }
+    }
+
+    public class TipoMovimientoRequest : TipoMovimientoBase
+    {
+        public Guid IdTipoMovimiento { get; set; }
+    }
+
+    public class TipoMovimientoResponse : TipoMovimientoBase
+    {
+        public Guid IdTipoMovimiento { get; set; }
+    }
+}
