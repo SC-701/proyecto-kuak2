e1ff0bedd51e13a2723f01d5dd3e63734ce3ea5c
Marco Bruno <135365238+marcongass@users.noreply.github.com>
2025-08-06 23:59:32 -0600

    final commit

--- QuantumBalance.API/API/Controllers/MovimientoController.cs
+++ QuantumBalance.API/API/Controllers/MovimientoController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,11 +9,97 @@ namespace API.Controllers
     [Route("api/[controller]")]
     [Authorize]
     [ApiController]
-    public class MovimientoController : Controller
+    public class MovimientoController : ControllerBase, IMovimientoController
     {
-        public IActionResult Index()
+        private IMovimientoFlujo _movimientoFlujo;
+        private ILogger<MovimientoController> _logger;
+
+        public MovimientoController(IMovimientoFlujo movimientoFlujo, ILogger<MovimientoController> logger)
         {
-            return View();
+            _movimientoFlujo = movimientoFlujo;
+            _logger = logger;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearMovimiento([FromBody] MovimientoRequest movimiento)
+        {
+            try
+            {
+                var res = await _movimientoFlujo.CrearMovimiento(movimiento);
+                return CreatedAtAction(nameof(ObtenerMovimientoPorId), new { id = res }, res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al crear el movimiento.");
+                return StatusCode(500, "Ocurrió un error al procesar la solicitud.");
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodosLosMovimientos()
+        {
+            try
+            {
+                var res = await _movimientoFlujo.ObtenerTodosLosMovimientos();
+                if (!res.Any())
+                    return NoContent();
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener los movimientos.");
+                return StatusCode(500, "Ocurrió un error al procesar la solicitud.");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObtenerMovimientoPorId([FromRoute] Guid id)
+        {
+            try
+            {
+                var res = await _movimientoFlujo.ObtenerMovimientoPorId(id);
+                if (res == null)
+                    return NotFound("Movimiento no encontrado.");
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener el movimiento por ID.");
+                return StatusCode(500, "Ocurrió un error al procesar la solicitud.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditarMovimiento([FromRoute] Guid id, [FromBody] MovimientoRequest movimiento)
+        {
+            try
+            {
+                var res = await _movimientoFlujo.EditarMovimiento(id, movimiento);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al editar el movimiento.");
+                return StatusCode(500, "Ocurrió un error al procesar la solicitud.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> EliminarMovimiento([FromRoute] Guid id)
+        {
+            try
+            {
+                var res = await _movimientoFlujo.EliminarMovimiento(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al eliminar el movimiento.");
+                return StatusCode(500, "Ocurrió un error al procesar la solicitud.");
+            }
+        }
+
+
+
+
     }
 }
