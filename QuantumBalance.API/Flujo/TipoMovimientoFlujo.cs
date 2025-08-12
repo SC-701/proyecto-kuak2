2fa9f8203982f0092b06a5e58643307dad8b4d9d
Sergio Mauricio Montenegro Pérez <156485337+sergioomp@users.noreply.github.com>
2025-08-12 12:55:10 -0600

    TipoMovimiento funcional

---
+++ QuantumBalance.API/Flujo/TipoMovimientoFlujo.cs
@@ -0,0 +1,29 @@
+﻿using Abstracciones.Interfaces.DA;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Flujo
+{
+    public class TipoMovimientoFlujo : ITipoMovimientoFlujo
+    {
+        private readonly ITipoMovimientoDA _tipoMovimientoDA;
+
+        public TipoMovimientoFlujo(ITipoMovimientoDA tipoMovimientoDA)
+        {
+            _tipoMovimientoDA = tipoMovimientoDA ?? throw new ArgumentNullException(nameof(tipoMovimientoDA));
+        }
+
+        public async Task<IEnumerable<TipoMovimientoResponse>> ObtenerTiposMovimiento()
+        {
+            return await _tipoMovimientoDA.ObtenerTiposMovimientos();
+        }
+
+        public async Task<TipoMovimientoResponse> ObtenerTipoMovimientoPorId(Guid idTipoMovimiento)
+        {
+            return await _tipoMovimientoDA.ObtenerTipoMovimientoPorId(idTipoMovimiento);
+        }
+    }
+}
